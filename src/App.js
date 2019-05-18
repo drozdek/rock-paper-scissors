91d480baed85b25e0039a2a1f2554d62d7eb032f
rafal <raf.drozdowski@gmail.com>
2019-05-18 18:19:52 +0100

    fix non immediate mutation of setState() add descriptive comments

--- src/App.js
+++ src/App.js
@@ -2,63 +2,108 @@ import React, { Component } from 'react';
 import Player from './Components/Player';
 import './App.css';
 
+/**
+ * @namespace App
+ * @extends React.Component
+ * @author [Rafal Drozdowski](https://rdrozdowski.github.io/)
+ */
 export default class App extends Component {
+
+  /**
+   * @constructor 
+   * 
+   * @param {string} props 
+   */
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+    };
     this.weapons = ["rock", "paper", "scissors"];
     this.runGame = this.runGame.bind(this);
   }
 
+  /**
+   * 
+   * @method runGame  starts game
+   * 
+   * @param none
+   */
   runGame() {
-    //this.checkWeaponStrength();    
-    console.log( this.state.playerWeapon1);
 
+    // sets states of weapons used
     this.setState({
       playerWeapon1: this.weapons[Math.floor(Math.random() * 3)],
       playerWeapon2: this.weapons[Math.floor(Math.random() * 3)],
     });
-    this.checkWeaponStrength();
-    //alert(this.state.playerWeapon1)
-    //console.log(this.state.playerWeapon1, this.state.playerWeapon2);
+
+    // fix non immediate mutation
+    // of setState method
+    setTimeout(() => {
+      this.checkWeaponStrength()
+    }, 0);
   }
 
+  /**
+   * @method checkWeaponStrength  check who has stronger weapon 
+   * 
+   * @params none
+   */
   checkWeaponStrength() {
-    const weapon1 = this.state.playerWeapon1;
-    const weapon2 = this.state.playerWeapon2;
+    const weapon1 = this.state.playerWeapon1,
+     weapon2 = this.state.playerWeapon2;
 
-    if ((weapon1 && weapon2)) {
+    // in case of weapons have the same values
+    if ((weapon1 === weapon2) || (weapon2 === weapon1)) {
       this.setState({
-        'winner': 'tie'
+        winner: 'tie'
       })
     }
+
+    // in case if paper interacts with scissors or opposite
     if ((weapon1 === "paper" && weapon2 === "scissors") ||
       (weapon1 === "scissors" && weapon2 === "paper")) {
       this.setState({
-        'winner': 'scissors'
+        winner: 'scissors'
       })
     }
+
+    // in case if paper interacts with rock or opposite
     if ((weapon1 === "paper" && weapon2 === "rock") ||
       (weapon1 === "rock" && weapon2 === "paper")) {
       this.setState({
-        'winner': 'paper'
+        winner: 'paper'
       })
     }
+
+    // in case if scissors interacts with rock or opposite
     if ((weapon1 === "scissors" && weapon2 === "rock") ||
       (weapon1 === "rock" && weapon2 === "scissors")) {
       this.setState({
-        'winner': 'rock'
+        winner: 'rock'
       })
-    }    
-
+    }
   }
 
+  /**
+   * @method render 
+   */
   render() {
     return (
       <div className="App">
-        <Player className="App__player" playerWeapon={this.state.playerWeapon1} />
-        <Player className="App__player" playerWeapon={this.state.playerWeapon2} />
+        {/**
+         *instantiate Player components & assign weapon
+         */}
+        <Player className="App__player App__player--one" playerWeapon={this.state.playerWeapon1} />
+        <Player className="App__player App__player--two" playerWeapon={this.state.playerWeapon2} />
+
+        {/**
+         * display result
+         */}
         <p>Battle has won: <b>{this.state.winner}</b></p>
+        
+        {/**
+        *show clickable button
+        */}
         <button className="App__button" onClick={this.runGame}> Play </button>
       </div>
     );
--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
+/**
+ * @namespace Player
+ * @extends React.Component
+ * @author [Rafal Drozdowski](https://rdrozdowski.github.io/)
+ */
 export default class Player extends Component {
+
+  /**
+   * @constructor  
+   * @param {string} props 
+   */
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
       <div className="App__player">
-        <div className="" >{this.props.playerWeapon}</div>
+
+      {/** 
+       * apply props playerWeapon 
+       */}      
+        <div>{this.props.playerWeapon}</div>
       </div>
     )
   }
