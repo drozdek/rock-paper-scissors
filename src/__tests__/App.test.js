057f2e9271e29e63dda2d9ef4bd64a023b36c4a8
rafal <raf.drozdowski@gmail.com>
2019-05-20 19:49:23 +0100

    add jest create snapshot update shapshot check if button exists

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ReactDOM from 'react-dom';
+import App from '../App';
+import { create } from 'react-test-renderer'
+
+describe('Rock, paper, scissors game', () => {
+  test('creates snapshot', () => {
+    let tree = create(<App />)
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has Play button', () => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.text().includes('Play')).toBe(true);
+  });
+
+});
+
